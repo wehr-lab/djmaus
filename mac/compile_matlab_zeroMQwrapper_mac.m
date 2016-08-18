7b4fff7505630cb30c4eea27464eaf828b7166a9
mikewehr <wehr@uoregon.edu>
2016-08-18 09:10:56 -0700

    Add files via upload
    rename compile files to mac/windows for clarity

---
+++ mac/compile_matlab_zeroMQwrapper_mac.m
@@ -0,0 +1,49 @@
+% compile 0MQ matlab wrapper
+%
+% NOTE: On OS X, you may need to implement this patch
+%       for compilation to work:
+%       http://www.mathworks.com/matlabcentral/answers/94092
+%
+
+
+% 1. make sure mex is setup properly and a compiler is available
+mex -setup
+
+% it helps to execute this from within the directory
+cd ('/Users/mikewehr/Documents/Analysis/plugin GUI/plugin-GUI/Resources/Matlab')
+%cd('D:\lab\djmaus\')
+
+%GUIfolder = 'C:\Users\Shay\Documents\GitHub\GUI';
+%GUIfolder = '/home/jsiegle/Programming/GUI/';
+GUIfolder = '/Users/mikewehr/Documents/Analysis/plugin GUI';
+%GUIfolder = 'D:\plugin-GUI-master';
+
+
+%headerFolder = [GUIfolder, '/Resources/windows-libs/ZeroMQ/include'];
+%headerFolder = [GUIfolder, '\Resources\windows-libs\ZeroMQ\include'];
+headerFolder = '/usr/local/include';
+
+if strcmp(computer,'PCWIN')
+    libFolder = [GUIfolder, '/Resources/ZeroMQ/lib_x86'];
+    libraryName = 'libzmq-v110-mt-3_2_2';
+    cppFile = 'windows/zeroMQwrapper.cpp';
+elseif strcmp(computer,'PCWIN64')
+    libFolder = [GUIfolder, '\Resources\windows-libs\ZeroMQ\lib_x64'];
+    %libraryName = 'libzmq-v110-mt-3_2_2';
+    libraryName = 'libzmq-v120-mt-4_0_4.lib';
+    cppFile = 'windows/zeroMQwrapper.cpp';
+    
+elseif strcmp(computer,'GLNX86') || strcmp(computer,'GLNXA64')
+    libFolder = '/usr/local/lib';
+    libraryName = 'zmq';
+    cppFile = 'unix/zeroMQwrapper.cpp';
+elseif strcmp(computer,'MACI64')
+    libFolder = '/opt/local/lib';
+    libraryName = 'zmq';
+    %cppFile = 'unix/zeroMQwrapper.cpp';
+    cppFile = 'mac/zeroMQwrapper.cpp';
+    
+end
+
+% 2. compile
+eval(['mex ' cppFile ' -I',headerFolder,' -L',libFolder,' -l',libraryName] )

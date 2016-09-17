0c0ba51bd0667e735425b9b5b3ad989ced815ae4
crimedude22 <JLSaunders987@gmail.com>
2016-09-16 17:26:45 -0700

    updates

--- windows/compile_matlab_zeroMQwrapper_win.m
+++ windows/compile_matlab_zeroMQwrapper_win.m
@@ -5,6 +5,17 @@
 %       http://www.mathworks.com/matlabcentral/answers/94092
 %
 
+% note: you will have to change the file locations below to match your
+% local machine configuration
+
+% note: on windows, check the output of mex -setup. If it's set up to use
+% the Visual Studio compiler -- "Building with 'Microsoft Visual C++ 2013
+% Professional'", you will probably not be able to compile because of VS
+% peculiarities with variable array size allocation. Instead, install the
+% MinGW g++ compiler by clicking the Add-Ons button on the matlab toolbar,
+% checking the Features box, and installing MinGW support. Then run mex
+% -setup and click the MinGW compiler link.
+%note: it's possible that restarting windows after re-compiling is helpful.
 
 % 1. make sure mex is setup properly and a compiler is available
 mex -setup
@@ -13,13 +24,14 @@ mex -setup
 % cd ('/Users/mikewehr/Documents/Analysis/plugin GUI/plugin-GUI/Resources/Matlab')
 cd('D:\lab\djmaus\')
 
+%Where is the plugin-GUI folder?
 %GUIfolder = 'C:\Users\Shay\Documents\GitHub\GUI';
 %GUIfolder = '/home/jsiegle/Programming/GUI/';
 %GUIfolder = '/Users/mikewehr/Documents/Analysis/plugin GUI';
-GUIfolder = 'D:\lab\plugin-GUI';
+%GUIfolder = 'D:\lab\plugin-GUI'; 
+GUIfolder = 'C:\lab\plugin-GUI';
 
 
-%headerFolder = [GUIfolder, '/Resources/windows-libs/ZeroMQ/include'];
 headerFolder = [GUIfolder, '\Resources\windows-libs\ZeroMQ\include'];
 %headerFolder = '/usr/local/include';
 

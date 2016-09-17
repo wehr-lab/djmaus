f48d1d182a1feb34133244dee45d4f53a0a0a827
crimedude22 <JLSaunders987@gmail.com>
2016-09-16 17:17:30 -0700

    updates

--- test_zeroMQ_wrapper.m
+++ test_zeroMQ_wrapper.m
@@ -6,12 +6,12 @@
 %instructions).
 
 url = 'localhost:5556'; % or, e.g., //'tcp://10.71.212.19:5556 if GUI runs on another machine...
-fprintf('\nopening thread to %s', url)
+fprintf('\n\nopening thread to %s', url)
 
 handle = zeroMQwrapper('StartConnectThread',url);
 fprintf('\nsuccess, handle is %g', handle)
 fprintf('\ntrying to send some messages...')
-zeroMQwrapper('Send',handle ,'ClearDesign');
+zeroMQwrapper('Send',handle ,'ChangeDirectory');
 zeroMQwrapper('Send',handle ,'NewDesign nGo_Left_Right');
 zeroMQwrapper('Send',handle ,'AddCondition Name GoRight TrialTypes 1 2 3');
 zeroMQwrapper('Send',handle ,'AddCondition Name GoLeft TrialTypes 4 5 6');
@@ -25,4 +25,8 @@ zeroMQwrapper('Send',handle ,'TrialEnd 1');
 
 zeroMQwrapper('CloseThread',handle);
 fprintf('\nIf you didn''t see any errors, zeroMQwrapper is probably working fine. ')
-fprintf('\nNote that this does not check to see if open-ephys actually received any of those messages. ')
+fprintf('\nNote that this does not check to see if open-ephys actually received ')
+fprintf('\nany of those messages. For that, check the open-ephys message console.')
+fprintf('\nIf you saw "Connection was closed. Cannot send", it means that')
+fprintf('\nthe zeroMQwrapper file probably works, but there was a problem')
+fprintf('\nopening the connection thread. In this case, try it again.')

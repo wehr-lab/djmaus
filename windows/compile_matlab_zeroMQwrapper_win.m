82f15c02c9123c594ad9a2936c2e7882f193930d
mikewehr <wehr@uoregon.edu>
2017-08-01 11:33:48 -0700

    compiling mex wrapper

--- windows/compile_matlab_zeroMQwrapper_win.m
+++ windows/compile_matlab_zeroMQwrapper_win.m
@@ -8,13 +8,13 @@
 % note: you will have to change the file locations below to match your
 % local machine configuration
 
-% note: on windows, check the output of mex -setup. If it's set up to use
-% the Visual Studio compiler -- "Building with 'Microsoft Visual C++ 2013
-% Professional'", you will probably not be able to compile because of VS
-% peculiarities with variable array size allocation. Instead, install the
+% note: on windows, check the output of mex -setup. YOu might need to install the mingw c++ compiler.
+% install the
 % MinGW g++ compiler by clicking the Add-Ons button on the matlab toolbar,
 % checking the Features box, and installing MinGW support. Then run mex
 % -setup and click the MinGW compiler link.
+% make sure it's set to use the C++ compiler, not the C compiler (which might be the default)
+%
 %note: it's possible that restarting windows after re-compiling is helpful.
 
 % 1. make sure mex is setup properly and a compiler is available
@@ -22,25 +22,29 @@ mex -setup
 
 % it helps to execute this from within the directory
 % cd ('/Users/mikewehr/Documents/Analysis/plugin GUI/plugin-GUI/Resources/Matlab')
-cd('D:\lab\djmaus\')
+cd('C:\lab\djmaus\')
 
 %Where is the plugin-GUI folder?
 %GUIfolder = 'C:\Users\Shay\Documents\GitHub\GUI';
 %GUIfolder = '/home/jsiegle/Programming/GUI/';
 %GUIfolder = '/Users/mikewehr/Documents/Analysis/plugin GUI';
-GUIfolder = 'D:\lab\plugin-GUI'; %wehrrig1
+GUIfolder = 'C:\lab\open-ephys-windows'; %wehrrig1
 %GUIfolder = 'C:\lab\plugin-GUI';
 
 
-headerFolder = [GUIfolder, '\Resources\windows-libs\ZeroMQ\include'];
+%headerFolder = [GUIfolder, '\Resources\windows-libs\ZeroMQ\include'];%probably for compiled
 %headerFolder = '/usr/local/include';
+headerFolder =    'C:\lab\ZeroMQ4.0.4\include'; %rig2, binary
+
 
 if strcmp(computer,'PCWIN')
     libFolder = [GUIfolder, '/Resources/ZeroMQ/lib_x86'];
     libraryName = 'libzmq-v110-mt-3_2_2';
     cppFile = 'windows/zeroMQwrapper.cpp';
 elseif strcmp(computer,'PCWIN64')
-    libFolder = [GUIfolder, '\Resources\windows-libs\ZeroMQ\lib_x64'];
+    %libFolder = [GUIfolder, '\Resources\windows-libs\ZeroMQ\lib_x64']; %probably for compiled
+    libFolder =   'C:\lab\ZeroMQ4.0.4\lib'; %rig2, binary download
+
     %libraryName = 'libzmq-v110-mt-3_2_2';
     libraryName = 'libzmq-v120-mt-4_0_4.lib';
     cppFile = 'windows/zeroMQwrapper.cpp';

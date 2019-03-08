fabcb45d4d3438325ad81c100373f56d4da4b4d8
Mike Wehr <wehr@uoregon.edu>
2019-03-08 13:45:46 -0800

    added Outfile_Combiner

---
+++ djtools/Outfile_Combiner.m
@@ -0,0 +1,6 @@
+function Outfile_Combiner
+
+%simple GUI to pick multiple outfiles and combine them
+%developed for ABRs (LFP tuning curves) so that we can split data
+%acquisition across multiple sessions
+

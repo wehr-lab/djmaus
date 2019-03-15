42fe9e3676afa3dafd30bc0a05f5310de5c5979e
tai-heinzerling <32080484+tai-heinzerling@users.noreply.github.com>
2019-03-14 18:44:52 -0700

    Update KSDirListBuilder.m

--- djtools/KSDirListBuilder.m
+++ djtools/KSDirListBuilder.m
@@ -7,6 +7,8 @@ function KSDirListBuilder(varargin)
 %
 % what does it do? It creates/appends a machine readable cell list as a
 % .txt file
+%
+%mw 3.14.2019
 
 global P
 
@@ -24,6 +26,7 @@ switch action
     case 'Init'
         InitializeGUI
     case 'Close'
+        delete(P.listviewer)
         delete(P.fig)
         clear global P
     case 'CreateNewCellList'
@@ -36,6 +39,13 @@ switch action
         BrowseAndAdd
     case 'TargetCellList'
         TargetCellList
+    case 'NextDataDir'
+        NextDataDir
+        set(P.pwdDisplay, 'string', pwd)
+    case 'PreviousDataDir'
+        PreviousDataDir
+        set(P.pwdDisplay, 'string', pwd)
+        
 end
 
 % Return the name of this function.
@@ -58,6 +68,7 @@ if fname
     P.TargetCellList=fullfile(path, fname);
     set(P.TargetCellListDisplay, 'string', {'cell list:',path, fname});
     set([P.BrowseAndAddh P.AddCurrentDirh], 'enable', 'on')
+    UpdateListviewer
 end
 
 function AddCurrentDir
@@ -87,7 +98,9 @@ end
 
 
 function BrowseAndAdd
+global P
 d = uigetdir(pwd, 'choose directory to scan')
+set(P.pwdDisplay, 'string', pwd)
 if d
     cd(d)
     AddCurrentDir
@@ -95,26 +108,32 @@ end
 
 function WriteToCellList(d)
 global P
-    
-        %here we can write out any additional stimulus or notebook info we want
-%     wd=pwd;
-%     [dd, ff]=fileparts(fullfile(pwd,(d(s).name)));
-%     cd(dd)
-%     try
-%         nb=load('notebook.mat');
-%         %here we can write out any additional stimulus or notebook info we want
-%         str=sprintf('%s\n%s',str, nb.stimlog(1).protocol_name);
-%         str=sprintf('%s\n', str);
-%     end
-%     cd(wd)
-response=questdlg(str, 'Write selected cells to file?', 'Write', 'Cancel', 'Write');
+
+%here we can write out any additional stimulus or notebook info we want
+%see CellListBuilder.m for examples
+response=questdlg(pwd, 'Write this data directory to file?', 'Write', 'Cancel', 'Write');
 switch response
     case 'Write'
         fid=fopen(P.TargetCellList, 'a'); %absolute path
         fprintf(fid, '\n\ndir');
         fprintf(fid, '\n%s', d);
         fclose(fid);
+        UpdateListviewer
 end
+
+function UpdateListviewer
+global P
+        fid=fopen(P.TargetCellList, 'r'); %absolute path
+     i=0;
+     while 1
+            tline = fgetl(fid);
+     if ~ischar(tline), break, end
+     i=i+1;
+            str{i}=tline;
+        end
+        fclose(fid);
+set(P.listviewerbox,'visible','on', 'string', str);
+figure(P.listviewer)
 
 function InitializeGUI
 
@@ -128,15 +147,20 @@ fig = figure;
 P.fig=fig;
 set(fig,'visible','off');
 set(fig,'visible','off','numbertitle','off','name','cell list builder',...
-    'doublebuffer','on','menubar','none','closerequestfcn','CellListBuilder(''Close'')')
+    'doublebuffer','on','menubar','none','closerequestfcn', [me '(''Close'');'])
 height=220; width=350; e=2; H=e;
 w=200; h=25;
-set(fig,'pos',[1000 800         width         height],'visible','on');
+set(fig,'pos',[800 800         width         height],'visible','on');
 
+P.listviewer=figure;
+set(P.listviewer,'pos',[1000 100 500 600],'visible','on');
+P.listviewerbox= uicontrol('parent',P.listviewer,'string','','tag','listviewerbox','units','pixels',...
+    'position',[1 1 500 600],'enable','inactive',...
+    'horiz', 'left', 'style','edit', 'max', 2 );
 
 %TargetCellList display
 P.TargetCellListDisplay= uicontrol('parent',fig,'string','','tag','TargetCellListDisplay','units','pixels',...
-    'position',[e H width-e 2*h],'enable','on',...
+    'position',[e H width-e 2*h],'enable','inactive',...
     'fontweight','bold','horiz', 'left',...
     'style','text');
 
@@ -154,8 +178,19 @@ uicontrol('parent',fig,'string','Select Existing Cell List','tag','SelectExistin
     'fontweight','bold',...
     'style','pushbutton','callback',[me ';']);
 
+% Next and Previous buttons
+H=H+h+e;
+P.Next=uicontrol('parent',fig,'string','Next Dir','tag','NextDataDir','units','pixels',...
+    'position',[e+w/2 H w/2 h],'enable','on',...
+    'fontweight','bold',...
+    'style','pushbutton','callback',[me ';']);
+P.Previous=uicontrol('parent',fig,'string','Previous Dir','tag','PreviousDataDir','units','pixels',...
+    'position',[e H w/2 h],'enable','on',...
+    'fontweight','bold',...
+    'style','pushbutton','callback',[me ';']);
+
 %Browse and Add button
-H=H+2*h+e;
+H=H+h+e;
 P.BrowseAndAddh=uicontrol('parent',fig,'string','Browse and Add','tag','BrowseAndAdd','units','pixels',...
     'position',[e H w h],'enable','off',...
     'fontweight','bold',...
@@ -168,116 +203,10 @@ P.AddCurrentDirh=uicontrol('parent',fig,'string','Add Current Dir','tag','AddCur
     'fontweight','bold',...
     'style','pushbutton','callback',[me ';']);
 
-% %recursive checkbox
-% H=H+h+e;
-% P.recursive=uicontrol('parent',fig,'string','Recursive Scan','tag','Recursive','units','pixels',...
-%     'position',[e H w h],'enable','on',...
-%     'style','checkbox');
 
-
-% function doOK(src, evt, IncludeCellcheckbox, sl, pvcheckbox)
-% 
-% for i=1:length(sl)
-%     IncludeCell(i)=get(IncludeCellcheckbox(i), 'value');
-%     ClustQual(i)=get(sl(i), 'value');
-%     PVcell(i)=get(pvcheckbox(i), 'value');
-% end
-% delete(gcbf);
-% global P
-% P.ok=1;
-% P.IncludeCell=IncludeCell;
-% P.ClustQual=ClustQual;
-% P.PVcell=PVcell;
-
-
-function doCancel(src, evt)
-% ad.selection = [];
-% ad.ClustQual=[];
-% ad.PVcell=[];
-% setappdata(0,'ListDialogAppData__',ad)
-delete(gcbf);
-global P
-P.ok=0;
-
-% % slider callback
-% function doSlider(sliderh, evd, sliderstrh) %#ok
-% set(sliderstrh, 'string', get(sliderh, 'value'))
-% 
-
-
-% function [selection, ok, ClustQual, PVcell]=myCellListDlg(d)
-% global P
-% 
-% fig=figure;
-% set(fig, 'pos',[800 150 500 800] )
-% 
-% selection=[];
-% ClustQual=[];
-% PVcell=[];
-% P.ok=0;
-% 
-% btn_wid=50;
-% btn_ht=25;
-% 
-% %     top=ffs+uh+4*fus+(smode==2)*(fus+uh)+listsize(2);
-% fontsize=12;
-% linesize = fontsize*1.4;  % height extent per line of uicontrol text (approx)
-% linespacing=linesize*1.5;
-% pos=get(fig, 'pos');
-% width=pos(3);
-% top=pos(4);
-% col1width=150; %cell name
-% col2width=50; %include cell checkbox
-% col3width=120; %cluster quality slider
-% col4width=50; %cluster quality numeric indicator
-% col5width=50; %pv cell checkbox
-% sliderwidth=col3width;
-% 
-% uicontrol('style', 'text', 'pos', [0, top-linesize, col1width, linesize], 'string', 'cell', ...
-%     'horizontalalignment', 'center', 'fontsize', fontsize)
-% uicontrol('style', 'text', 'pos', [col1width, top-linesize, col2width, linesize], 'string', 'include?', ...
-%     'horizontalalignment', 'center', 'fontsize', fontsize)
-% uicontrol('style', 'text', 'pos', [col1width+col2width, top-linesize, sliderwidth, linesize],...
-%     'string', 'cluster quality', 'fontsize', fontsize)
-% uicontrol('style', 'text', 'pos', [col1width+col2width+col3width+col4width, top-linesize, 50, linesize], ...
-%     'string', 'PV cell?', 'fontsize', fontsize, 'horizontalalign', 'left')    
-% 
-% for i=1:length(d)
-%     if ~mod(i,3)
-%     u=uipanel('units', 'pixels', 'pos',[ 0,top-(i+2)*linespacing-3, width, 2]); 
-%     end
-%     cellstr(i)=uicontrol('style', 'text', 'pos',[2, top-(i+2)*linespacing, col1width, linesize],...
-%          'horizontalalignment', 'right','fontsize', fontsize,'string', d(i).name); %cell name
-%     IncludeCellcheckbox(i)=uicontrol('style', 'checkbox', 'pos', ...
-%         [col1width, top-(i+2)*linespacing, 30, linesize], 'value', 1); %include cell checkbox
-%     slstr(i)=uicontrol('style', 'text', 'pos', ...
-%         [col1width+col2width+col3width+2, top-(i+2)*linespacing, 10, linesize], 'string', 0); %cluster quality numeric indicator
-%     sl(i)=uicontrol('style', 'slider', 'pos', [col1width+col2width, top-(i+2)*linespacing, sliderwidth, linesize], ...
-%         'min', 0, 'max', 5, 'sliderstep', [.2 .2], 'value', 0, 'callback', {@doSlider, slstr(i)} ); %cluster quality slider
-%     pvcheckbox(i)=uicontrol('style', 'checkbox', 'pos', ...
-%         [col1width+col2width+col3width+col4width, top-(i+2)*linespacing, 30, linesize]); %pvcheckbox
-% end
-% 
-% ok_btn = uicontrol('Style','pushbutton',...
-%     'String','OK',...
-%     'Position',[10 10 btn_wid btn_ht],...
-%     'Tag','ok_btn',...
-%     'Callback',{@doOK,IncludeCellcheckbox, sl, pvcheckbox });
-% 
-% cancel_btn = uicontrol('Style','pushbutton',...
-%     'String','Cancel',...
-%     'Position',[20+btn_wid 10 btn_wid btn_ht],...
-%     'Tag','cancel_btn',...
-%     'Callback',@doCancel);
-% 
-%  uiwait(fig)
-%  ok=P.ok;
-%  if ok
-%      selection=P.IncludeCell;
-%      ClustQual=P.ClustQual;
-%      PVcell=P.PVcell;
-% else
-%     selection=[];
-%     ClustQual=[];
-%     PVcell=[];
-% end
+%pwd display
+H=H+h;
+P.pwdDisplay= uicontrol('parent',fig,'string',pwd,'tag','pwdDisplay','units','pixels',...
+    'position',[e H width-e h],'enable','inactive',...
+    'fontweight','bold','horiz', 'left',...
+    'style','text');

d828c8d89589ac6b4d088f32a86133c961d876d3
sfm <sammehan6@gmail.com>
2025-08-21 17:19:48 -0700

    Create MakeSquare.m

---
+++ MakeStimuli/MakeSquare.m
@@ -0,0 +1,47 @@
+function sqWave=MakeSquare(varargin)
+% usage: sqWave=MakesqWave(params, samplerate)
+%inputs:
+% params (should have the following fields:)
+%   duty            -   percent of cycle positive
+%   amplitude          -   sound pressure level of the tone (dB)
+%   duration           -   duration of the square wave (us)
+% samplerate         -    sampling rate in hz
+
+% Note: we use absolute SPL instead of attenuation!!!
+% patterned after: tone=MakeTone(params, samplerate)
+%%%% NOT SURE IF sqWave HAS TO START AND END WITH 0 ???
+
+global pref
+
+% Creates a squarewave of a given duty cycle, attenuation, duration, at a
+% given samplerate
+% duty is percent positive
+% duration is in us
+% amplitude in dB
+%
+%  Output:
+%  sqWave               -   the specified squarewave (empty if unsuccessful)
+%
+
+sqWave=[];
+
+if nargin<2
+    return;
+end
+
+params=varargin{1};
+samplerate=varargin{2};
+duty=params.duty;
+amplitude=params.amplitude;
+duration=params.duration;
+
+
+amplitude=1*(10.^((amplitude-pref.maxSPL)/20)); %in volts (-1<x<1), i.e. pref.maxSPL=+_1V
+duration=duration/1000000;                     % adjust the duration to seconds
+t = 0:1/samplerate:duration-1/samplerate;   % length of the sampled trial
+sqWave=amplitude*square(2*pi/duration*t,duty);       % the new squareWave itself
+
+% figure
+% plot(0:1/30:params.duration-1/30,sqWave)
+% xlabel('Time (ms)')
+% ylabel('amplitude')
